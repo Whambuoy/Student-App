37f479df023ff8fa6d6a13b2ebe93e03b3bb4a83
Whambuoy <esther25maina@gmail.com>
2018-11-22 11:08:16 +0300

    changes 1

--- app/src/main/java/com/example/whambuoy/myapplication/Login.java
+++ app/src/main/java/com/example/whambuoy/myapplication/Login.java
@@ -2,12 +2,43 @@ package com.example.whambuoy.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    EditText editFullName;
+    EditText editPassword;
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editFullName = (EditText) findViewById(R.id.editFullName);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateCredentials();
+            }
+        });
+    }
+
+    public void validateCredentials(){
+        String fullname = editFullName.getText().toString();
+        String password = editPassword.getText().toString();
+
+        if(fullname.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+        }
     }
 }
